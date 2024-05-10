9e864ef025fe118f0f2a731bbce0b5ba36914b7f
abhishek kumar <92795729+Abhi19990628@users.noreply.github.com>
2024-05-11 00:03:16 +0530

    Update models.py

--- dashboard/models.py
+++ dashboard/models.py
@@ -5,7 +5,7 @@ class DataPoint(models.Model):
     likelihood = models.CharField(max_length=100)
     relevance = models.CharField(max_length=100)
     year = models.IntegerField(null)
-    country = models.CharField(max_length=100)
+    
     topics = models.CharField(max_length=100)
     region = models.CharField(max_length=100)
     city = models.CharField(max_length=100)
